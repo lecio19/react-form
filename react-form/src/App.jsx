0f678777342f9d180b990cee09099e14ac9143b4
lecio19 <alex.macri01@gmail.com>
2024-12-02 15:46:44 +0100

    add blocchi di codice main part

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./src/components/header.jsx"
+import Footer from "./src/components/Footer.jsx"
+import Lavagna from "./src/components/Lavagna.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
